480abb233dd1543708576da1e19a3a74209ca11b
samuelcpr <samuelcpr464@gmail.com>
2023-07-20 14:31:19 -0300

    chore: mudando relatórios padrão do sistema

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -25,9 +25,9 @@ import './Status.css';
 export function Status() {
   const [newAnswer, setNewAnswer] = useState('')
   const [answers, setAnswers] = useState([
-    'Concordo...',
-    'Olha, faz sentido!',
-    'Parabéns pelo progresso.'
+    'Ocorrência de Pragas (Soja):  Hoje, foram encontradas cigarrinhas em 5 % das plantas de soja.Pulgões foram identificados em 10 % das áreas monitoradas, e percevejos em 2 %.Medidas de controle foram acionadas conforme necessidade.',
+    'Aplicação de Medidas (Soja): Neste relatório, registramos a aplicação de inseticida biológico para combater as cigarrinhas, visando reduzir sua população. Além disso, foram realizadas armadilhas amarelas para monitorar e controlar os pulgões. Medidas naturais de controle estão sendo avaliadas para minimizar o impacto ambiental.',
+    'Monitoramento de Pragas (Soja): O monitoramento das áreas infestadas por percevejos mostrou uma tendência de aumento nos últimos dias. Recomendamos intensificar a vigilância nesses locais e avaliar a adoção de práticas culturais para controlar essa praga, como a rotação de culturas e o manejo integrado de pragas. Continuaremos a observar o comportamento das pragas e a eficácia das medidas implementadas.'
   ])
 
   function createNewAnswer(event: FormEvent) {
@@ -46,18 +46,17 @@ export function Status() {
 
   return (
     <main className="status">
-      <Header title="Tweet" />
 
-      <Tweet content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique officia autem libero, veritatis mollitia qui nisi eveniet cumque rerum atque, unde nobis, consectetur illo assumenda. Amet totam consequuntur repudiandae cum?" />
-
-      <Separator />
+      
+      <h1 className="text-header-page">Relatórios de observações</h1>
+    
 
       <form onSubmit={createNewAnswer} className="answer-tweet-form">
         <label htmlFor="tweet">
           <img src="https://avatars.githubusercontent.com/u/79328967?v=4" alt="Samuel Carlos" />
-          <textarea 
-            id="tweet" 
-            placeholder="Tweet your answer" 
+          <textarea
+            id="tweet"
+            placeholder="Criar Observação"
             value={newAnswer}
             onKeyDown={handleHotkeySubmit}
             onChange={(event) => {
@@ -68,7 +67,7 @@ export function Status() {
 
         <button type="submit">
           <PaperPlaneRight />
-          <span>Answer</span>
+          <span>Enviar</span>
         </button>
       </form>
 
